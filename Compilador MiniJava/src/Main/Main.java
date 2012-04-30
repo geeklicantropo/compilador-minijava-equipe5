1e90b370938ceacc358e7f35fc4b91fad27ed132
sanmoo.cs <sanmoo.cs@gmail.com>
2012-04-30 19:01:08 +0000

    Árvore Sintática Abstrata.

--- Compilador MiniJava/src/Main/Main.java
+++ Compilador MiniJava/src/Main/Main.java
@@ -1,17 +1,35 @@
 package Main;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
+import syntaxtree.Program;
+import visitor.PrettyPrintVisitor;
 import Parser.MiniJavaParser;
 import Parser.ParseException;
-import syntaxtree.*;
-import visitor.*;
 
 public class Main {
-   public static void main(String [] args) {
-      try {
-          Program root = new MiniJavaParser(System.in).Program();
-          root.accept(new PrettyPrintVisitor());
-      }
-      catch (ParseException e) {
-         System.out.println(e.toString());
-      }
-   }
+	public static void main(String [] args) {
+		Reader in;
+		if (args.length==0) { 
+			System.out.println("Digite o caminho do arquivo como argumento para o programa");
+			return;
+		}
+		try {
+			System.out.println("Compilando... "+args[0]);
+			in = new FileReader(args[0]);
+		} catch (FileNotFoundException e1) {
+			System.out.println("Arquivo nao encontrado.");
+			e1.printStackTrace();
+			return;
+		}
+		try {
+			MiniJavaParser parser = new MiniJavaParser(in);
+            Program root = MiniJavaParser.Program();
+			root.accept(new PrettyPrintVisitor());
+		}
+		catch (ParseException e) {
+			System.out.println(e.toString());
+		}
+	}
 }
